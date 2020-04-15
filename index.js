521ef47f910aa2694a6357f992fe258676a17ab1
Grace229 <itiunggrace@gmail.com>
2020-04-15 21:13:58 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const users = require("./user");
 const app = express()
-const port = 4000
-app.listen(port,() => console.log(`listening on ${port} ...`));
 app.get('/',(req, res) => {
     res.send('Welcome');
 });
 app.get('/api/user', (req, res) => {
-    res.status(200).json({
-        users,
-    });
+    res.send(users)
+      
+
 });
-module.exports
+const port = 4000
+app.listen(port,() => console.log(`listening on ${port} ...`));
+module.exports 
